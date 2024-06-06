178869ab5abc412275bda9c5fcff65ed7348f981
Shubham <sdeodhar47@gmail.com>
2024-06-07 01:05:37 +0530

    added dummy tests

---
+++ app/app/tests.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from app.calc import add_nums
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_nums(self):
+        res = add_nums(9,10)
+        self.assertEqual(19, res)

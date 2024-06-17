2bc6e4318872a2e5905447f6698785a263cc475d
Shubham <sdeodhar47@gmail.com>
2024-06-18 01:20:26 +0530

    normalized the email and added testcase to normalize the email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,14 @@ class ModelTests(TestCase):
         )
         self.assertEqual(email, user_model.email)
         self.assertTrue(user_model.check_password(password))
+        
+    def test_new_user_email_normalized(self):
+        """test email is normalised for new users"""
+        sample_emails = [
+            ['test1@Example.com', 'test1@example.com'],
+            ['Test2@example.com', 'Test2@example.com'],
+            ['Test3@EXAMPLE.com', 'Test3@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample")
+            self.assertEqual(user.email, expected)
